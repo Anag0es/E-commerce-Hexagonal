a2c66ead7eb9ebbd9e3feb4046ac3e82449866a4
anag0es <analuisa.goes2004@gmail.com>
2025-01-08 17:19:08 -0300

    fix: dominio Pedido

--- src/main/java/com/arquitetura/hexagonal/domain/model/Pedido.java
+++ src/main/java/com/arquitetura/hexagonal/domain/model/Pedido.java
@@ -7,7 +7,35 @@ public class Pedido {
 
     private long id;
     private Cliente cliente;
-    private List<Produto> quantidade;
-    private List<Produto> produtos;
+    private int quantidade;
+    private List<Long> produtos;
     private LocalDateTime dataPedido;
+
+    public Pedido(long id, Cliente cliente, int quantidade, List<Long> produtos, LocalDateTime dataPedido) {
+        this.id = id;
+        this.cliente = cliente;
+        this.quantidade = quantidade;
+        this.produtos = produtos;
+        this.dataPedido = dataPedido;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public List<Long> getProdutos() {
+        return produtos;
+    }
+
+    public LocalDateTime getDataPedido() {
+        return dataPedido;
+    }
 }
